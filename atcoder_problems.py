3ac6f46f28a343d7dd47adfb4b60a4fdf0589504
reverberate <raphaellimfuencoding@gmail.com>
2024-06-03 21:12:54 +0800

    commit2

--- atcoder_problems.py
+++ atcoder_problems.py
@@ -1,27 +1,27 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium import webdriver
+#from selenium.webdriver.chrome.options import Options
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from models import question, problem_set
+from playwright.sync_api import sync_playwright
+
+def fetch_html(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+        # Wait for the content to load
+        page.wait_for_load_state('networkidle')
+        content = page.content()
+        browser.close()
+        return content
 
 def atcoder_propagate(problem_sets, username):
     #Accesing the URL
     url = f"https://kenkoooo.com/atcoder/#/table/{username}"
-
-
-    driver = webdriver.Chrome()
-
-    driver.get(url)
-
-    wait = WebDriverWait(driver, 10)
-    #wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table-problem")))
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table-success")))
-    html = driver.page_source
-
-    driver.quit()
-
+    html = fetch_html(url=url)
     soup = BeautifulSoup(html,'html.parser')
 
     niggas = soup.find_all(class_="table-success")
