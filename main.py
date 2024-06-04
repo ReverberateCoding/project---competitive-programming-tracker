b989fe4d9978aa44b1a56c9fb186606a2d56b3d4
reverberate <raphaellimfuencoding@gmail.com>
2024-06-04 15:40:35 +0800

    commit3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from atcoder_problems import atcoder_propagate
 from models import question, problem_set
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 #problem_sets = list()
 #atcoder_propagate(problem_sets=problem_sets, username="shoryu386")
 
@@ -12,11 +12,15 @@ with open("problemsets.txt", "w", encoding="utf-8") as file:
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/")
-def home():
-    return "Please add / alongside your AtCoder username to the current link"
+@app.route("/", methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        return redirect(url_for('index', username=username))
 
-@app.route("/<username>")
+@app.route("/<username>", methods=['POST', 'GET'])
 def index(username):
     problem_sets = list()
     atcoder_propagate(problem_sets=problem_sets, username=username)
---
+++ static/script.js
@@ -0,0 +1,6 @@
+let submit_button = document.getElementById("submit_button")
+
+submit_button.addEventListener("click", ()=>{
+    let text1 = document.getElementById('text1')
+    text1.innerHTML = "Loading statistics..."
+});
